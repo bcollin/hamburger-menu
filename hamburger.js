b3382fc4142f7a7d857fb2254209acf3b89e64cd
Branko Collin <collin@xs4all.nl>
2020-12-11 13:45:13 +0100

    Coding style and readability.

--- hamburger.js
+++ hamburger.js
@@ -1,22 +1,21 @@
 
-function menuHamburger(id, localStrings) {
+function menuHamburger( id, localStrings ) {
 	
-	var menuContainer = document.getElementById(id);
+	var menuContainer = document.getElementById( id );
 	
-	if (null === menuContainer) {
+	if ( null === menuContainer ) {
 		return false;
 	}
 	
 	menuContainer.classList.add( 'menu-hamburger-hidden' );
-
 	
 	var defaultStrings = {'showmenu': 'Show menu', 'hidemenu': 'Hide menu'};
 	
-	if (undefined === localStrings) {
+	if ( undefined === localStrings ) {
 		localStrings = defaultStrings; 
 	} else {
-		for (var s in defaultStrings) {
-			if (undefined === localStrings[s]) {
+		for ( var s in defaultStrings ) {
+			if ( undefined === localStrings[s] ) {
 				localStrings[s] = defaultStrings[s];
 			}
 		}
@@ -24,19 +23,19 @@ function menuHamburger(id, localStrings) {
 	
 	// Build the icon.
 	var toggleIcon = document.createElement('div');
-	toggleIcon.setAttribute('class', 'icon');
+	toggleIcon.setAttribute( 'class', 'icon' );
 	toggleIcon.innerHTML = '<span class="icon-show"><span class="label">' + localStrings['showmenu'] + '</span></span><span class="icon-hide"><span class="label">' + localStrings['hidemenu'] + '</span></span>';
-	menuContainer.insertBefore(toggleIcon, menuContainer.childNodes[0]);
+	menuContainer.insertBefore( toggleIcon, menuContainer.childNodes[0] );
 	
-	// Capture and process clicks/taps.
-	toggleIcon.addEventListener('click', function(){
+	// Capture and process clicks or taps.
+	toggleIcon.addEventListener( 'click', function(){
 		if ( menuContainer.classList.contains( 'menu-hamburger-hidden' ) ) {
 			menuContainer.classList.remove( 'menu-hamburger-hidden' );
 		}
 		else {
 			menuContainer.classList.add( 'menu-hamburger-hidden' );
 		}
-	});
+	} );
 		
 	return true;
 }
